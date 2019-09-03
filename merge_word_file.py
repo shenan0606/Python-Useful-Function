a8310936dd9b502f36b5de8ae43f7eec06787810
shenan0606 <50146467+shenan0606@users.noreply.github.com>
2019-09-03 13:54:55 -0400

    Create merge_word_file.py
    Merge Microsoft word document together
    Code form https://stackoverflow.com/questions/24872527/combine-word-document-using-python-docx

---
+++ merge_word_file.py
@@ -0,0 +1,20 @@
+from docx import Document
+
+files = ['file1.docx', 'file2.docx']
+
+def combine_word_documents(files):
+    merged_document = Document()
+
+    for index, file in enumerate(files):
+        sub_doc = Document(file)
+
+        # Don't add a page break if you've reached the last file.
+        if index < len(files)-1:
+           sub_doc.add_page_break()
+
+        for element in sub_doc.element.body:
+            merged_document.element.body.append(element)
+
+    merged_document.save('merged.docx')
+
+combine_word_documents(files)
