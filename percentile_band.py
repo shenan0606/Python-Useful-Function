baa5fd59b0d811a45f1c04a1a9b26ff7a08b1caa
shenan0606 <50146467+shenan0606@users.noreply.github.com>
2019-09-04 11:01:02 -0400

    Update percentile_band.py

--- percentile_band.py
+++ percentile_band.py
@@ -1,4 +1,7 @@
 # Given the Percentile of the score and the size of the band, assign score in each band
+import numpy as np
+import pandas as pd
+
 def percentile_band(percentile, band_size = 10):
     percentile = np.array(percentile)
     band = np.ceil(percentile / (100 / band_size))
