73fdcac73e7a2c4cfeeeeeefa0a9599770a495bb
shenan0606 <50146467+shenan0606@users.noreply.github.com>
2019-09-04 10:48:26 -0400

    Create percentile_band.py

---
+++ percentile_band.py
@@ -0,0 +1,11 @@
+# Given the Percentile of the score and the size of the band, assign score in each band
+def percentile_band(percentile, band_size = 10):
+    percentile = np.array(percentile)
+    band = np.ceil(percentile / (100 / band_size))
+    band = np.where(band == 0, 1, band)
+    return band
+    
+df = pd.DataFrame({'Percentile':[i for i in range(101)]})
+df['Decile'] = percentile_band(df['Percentile'], band_size = 10)
+df['Twentile'] = percentile_band(df['Percentile'], band_size = 20)
+print(df[['Percentile', 'Decile', 'Twentile']])
